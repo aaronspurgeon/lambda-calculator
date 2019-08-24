1a33698f22e3f115e25f65a5f9ce98494fb9cacd
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-08-23 20:52:02 -0400

    got stuck with the operators button component, I have the specials operator rendoring

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-const SpecialButton = () => {
+const SpecialButton = (props) => {
+  const { item } = props;
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button className="btn" >{item}</button>
     </>
   );
 };
+
+export default SpecialButton;
