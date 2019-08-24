967bb8f51da3377217fbe1c1a39f48e36fbcd0f4
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-08-23 20:26:52 -0400

    working on getting the operators component rendering

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import "./App.css";
 
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
+import Display from "./components/DisplayComponents/Display";
 import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
+import Operators from "./components/ButtonComponents/OperatorButtons/Operators";
 
 
 function App() {
@@ -21,6 +23,7 @@ function App() {
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
         <Numbers />
+        <Operators />
       </div>
     </div>
   );
