8e1af918fe0fdeef99b32f44602b0af6d6b67f28
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-08-23 20:55:14 -0400

    i have the operator component rendoring properly

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -15,8 +15,8 @@ const Operators = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
-       {operatorState.map((item) => {
-         return <OperatorButton  key={item.char} text={item.value}/>
+       {operatorState.map((item, index) => {
+         return <OperatorButton  key={index} item={item.char} value={item.value}/>
        })}
     </div>
   );
